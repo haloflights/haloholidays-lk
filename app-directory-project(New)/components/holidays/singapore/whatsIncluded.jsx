134df411acac35759413731184d68e6ead2eaffd
haloflights <haloflights38@gmail.com>
2023-12-21 19:07:43 +0530

    icon change

--- app-directory-project(New)/components/holidays/singapore/whatsIncluded.jsx
+++ app-directory-project(New)/components/holidays/singapore/whatsIncluded.jsx
@@ -9,7 +9,7 @@ const WhatsIncluded = () => {
     <img alt="Hotel" src="\img\icons\Hotel.svg" className="px-1"/>  <br/>  <span className="text-12 f2-500 " > Hotel</span>
     </div> +
     <div className="col hotel">
-    <img alt="Flight" src="\img\icons\flight.svg" /> <br/><span className="text-12"> Flight</span>
+    <img alt="Flight" src="\img\icons\Flight1.svg" /> <br/><span className="text-12"> Flight</span>
     </div> +
     <div class="col hotel ">     
     <img alt="Bag" src="\img\icons\Bag.svg" /><br/> <span className="text-12"> 23Kg bags</span>
